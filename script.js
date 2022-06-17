86e499f0c04137370f24b975abd52b8b332e1305
Davron <davron1104@yandex.com>
2022-06-17 17:23:56 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// jshint esversion: 6
+"use strict"
+
+// Fibonacci 
+function fibonacciTask(n) {
+	let output = [];
+	if(typeof(n) !== 'number' || !Number.isInteger(n) || n === 0) {
+		return '';
+	}
+	if(n === 1) {
+		output = [0];
+	} else if (n === 2) {
+		output = [0, 1];
+	} else {
+		output = [0, 1];
+		for(let i = 2; i < n; i++) {
+			output.push(output[output.length - 2] + output[output.length - 1]);
+		}
+	}
+
+	return output.join(' ');
+}
+
+output = fibonacciTask(100);
+console.log(output);
